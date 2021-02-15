f7ff37c9bf31551c05a997b51b045d5187468b14
lg13826 <47194138+lg13826@users.noreply.github.com>
2021-02-15 15:51:52 -0800

    Main file updated
    W.I.P.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <fstream>
+//include additional function files here
+
 
 int main()
 {
+	std::string entity;
+	std::ifstream inputf("input.txt");
+	std::ofstream outputf("output.txt");
+	outputf << "TOKENS" << '\t' << '\t' << "Lexemes" << std::endl << std::endl;
 
+	if (inputf.fail()) // error check, no input file detected and new one is created.
+	{
+		inputf.close();
+		std::ofstream inputf("input.txt");
+		return 1;
+	}
 
+	while (inputf.eof() == false) // go through input.txt and read each word.
+	{
+		int type;
+		inputf >> entity;
+
+		if (isKeyword(entity))
+		{
+			outputf << "KEYWORD" << '\t' << '=' << '\t';
+		}
+		else if (isIdentifier(entity))
+		{
+			outputf << "IDENTIFIER" << '\t' << '=' << '\t';
+		}
+		else if (isSeperator(entity))
+		{
+			outputf << "SEPERATOR" << '\t' << '=' << '\t';
+		}
+		else if (isOperator(entity))
+		{
+			outputf << "OPERATOR" << '\t' << '=' << '\t';
+		}
+
+		outputf << entity << std::endl;		
+	}
+
+	inputf.close();
 	return 0;
 }
